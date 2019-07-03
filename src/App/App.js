fc6e0cc723dcd967b2d92355f3bfccd24c6088fb
Jennifer Lipps <jjlipps@gmail.com>
2019-07-02 19:27:15 -0500

    private public routes working

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 import firebase from 'firebase/app';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from '../components/Home/Home';
 import Auth from '../components/Auth/Auth';
@@ -9,6 +16,22 @@ import fbConnection from '../helpers/data/connection';
 import './App.scss';
 
 fbConnection();
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (authed === false
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />)
+  );
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (authed === true
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />)
+  );
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
 
 class App extends React.Component {
   state = {
@@ -31,17 +54,23 @@ class App extends React.Component {
 
   render() {
     const { authed } = this.state;
-    const loadComponent = () => {
-      if (authed) {
-        return <Home />;
-      }
-      return <Auth />;
-    };
 
     return (
       <div className="App">
-        <MyNavBar authed={authed} />
-        {loadComponent()}
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavBar authed={authed} />
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <PublicRoute path='/auth' component={Auth} authed={authed} />
+                  <PrivateRoute path='/home' component={Home} authed={authed} />
+                  <Redirect from="*" to="/auth" />
+                </Switch>
+              </div>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
