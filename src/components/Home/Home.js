b1f6bedaf73d9a03e9fcd2fdc217da5a6b99638a
Jennifer Lipps <jjlipps@gmail.com>
2019-07-08 19:41:00 -0500

    get complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import scatData from '../../helpers/data/scatData';
 
 import './Home.scss';
 
 class Home extends React.Component {
+ state = {
+   scats: [],
+ }
+
+ componentDidMount() {
+   const { uid } = firebase.auth().currentUser;
+   scatData.getScats(uid)
+     .then(scats => this.setState({ scats }))
+     .catch(err => console.error('no scats in Home', err));
+ }
+
  editEvent = (e) => {
    e.preventDefault();
    const orderId = '12345';
@@ -11,12 +25,15 @@ class Home extends React.Component {
  }
 
  render() {
-   const singleLink = '/scat/12345';
+   //  const singleLink = '/scat/12345';
+   const showScats = this.state.scats.map(scat => (
+     <h2>{scat.sampleName}</h2>
+   ));
+
    return (
       <div className="Home">
         <h2>Home</h2>
-        <button className="btn btn-warning" onClick={this.editEvent}>Edit</button>
-        <Link to={singleLink}>View Single</Link>
+        { showScats }
       </div>
    );
  }
