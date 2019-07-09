2e5489c68bb222a914b612c69aeaf8ffea664ce5
Jennifer Lipps <jjlipps@gmail.com>
2019-07-09 18:53:43 -0500

    add form updates state

--- src/components/NewScat/NewScat.js
+++ src/components/NewScat/NewScat.js
@@ -2,11 +2,98 @@ import React from 'react';
 
 import './NewScat.scss';
 
+const defaultScat = {
+  location: '',
+  weight: '',
+  color: '',
+  sampleName: '',
+  animal: '',
+};
+
 class NewScat extends React.Component {
+  state = {
+    newScat: defaultScat,
+  }
+
+  formFieldStringState = (name, e) => {
+    const tempScat = { ...this.state.newScat };
+    tempScat[name] = e.target.value;
+    this.setState({ newScat: tempScat });
+  }
+
+  locationChange = e => this.formFieldStringState('location', e);
+
+  weightChange = e => this.formFieldStringState('weight', e);
+
+  sampleNameChange = e => this.formFieldStringState('sampleName', e);
+
+  colorChange = e => this.formFieldStringState('color', e);
+
+  animalChange = e => this.formFieldStringState('animal', e);
+
   render() {
+    const { newScat } = this.state;
     return (
       <div className="NewScat">
-        <h2>NewScat</h2>
+        <h1>New Scat</h1>
+        <form>
+          <div className="form-group">
+            <label htmlFor="sampleName">Location</label>
+              <input
+              type="text"
+              className="form-control"
+              id="location"
+              placeholder="place"
+              value={newScat.location}
+              onChange={this.locationChange}
+              />
+          </div>
+          <div className="form-group">
+            <label htmlFor="sampleName">Weight</label>
+              <input
+              type="text"
+              className="form-control"
+              id="weight"
+              placeholder="12g"
+              value={newScat.weight}
+              onChange={this.weightChange}
+              />
+          </div>
+          <div className="form-group">
+            <label htmlFor="sampleName">Sample</label>
+              <input
+              type="text"
+              className="form-control"
+              id="sampleName"
+              placeholder="Sample 12"
+              value={newScat.sampleName}
+              onChange={this.sampleNameChange}
+              />
+          </div>
+          <div className="form-group">
+            <label htmlFor="color">Color</label>
+              <input
+              type="text"
+              className="form-control"
+              id="color"
+              placeholder="brown"
+              value={newScat.color}
+              onChange={this.colorChange}
+              />
+          </div>
+          <div className="form-group">
+            <label htmlFor="color">Animal</label>
+              <input
+              type="text"
+              className="form-control"
+              id="animal"
+              placeholder="fox"
+              value={newScat.animal}
+              onChange={this.animalChange}
+              />
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
       </div>
     );
   }
